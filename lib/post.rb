10137605c1b38bd78355c7fe5d2de07969b618b3
Katie Moosman <moosman.katie@gmail.com>
2018-12-23 02:56:27 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post 
+  attr_accessor :title
+  
+  @@all = []
+ 
+  def initialize(title)
+    @title = title
+    # @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end  
+  
+  def artist_name
+    if @artist != nil
+      self.artist.name
+    else
+      nil
+    end
+  end
+  
+end
