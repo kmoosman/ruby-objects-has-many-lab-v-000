14f096d668a30300a09a43c67b0b61b942cf944c
Katie Moosman <moosman.katie@gmail.com>
2018-12-23 02:55:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,7 @@ class Artist
   
   def add_song_by_name(song_name)
      song = Song.new(song_name)
-    @songs << song
+     @songs << song
      song.artist = self
      @@song_count += 1
    end
